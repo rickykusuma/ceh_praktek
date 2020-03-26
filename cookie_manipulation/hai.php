5cb19a4d76a08bc1980c43d467f89cc73ebfb91c
albetz99 <albetz@gmail.com>
2020-03-26 15:50:34 +0700

    clean code

--- cookie_manipulation/hai.php
+++ cookie_manipulation/hai.php
@@ -1,20 +1,20 @@
-		<?php
-		include '../main/conn.php';
+<?php
+	include '../main/conn.php';
 
-		if (isset($_COOKIE['sid'])) {
-		    $sid = $_COOKIE['sid'];
-		    $sql = "SELECT * FROM users WHERE sid = '".$sid."'";
-		    $result = $conn->query($sql);
+	if (isset($_COOKIE['sid'])) {
+		$sid = $_COOKIE['sid'];
+		$sql = "SELECT * FROM users WHERE sid = '".$sid."'";
+		$result = $conn->query($sql);
 
-		    while($row = $result -> fetch_assoc())
-				$main = "Welcome ".$row['name'];
-		} 
-		else 
-			echo "<script>window.location.href='index.php'</script>";
+		while($row = $result -> fetch_assoc())
+			$main = "Welcome ".$row['name'];
+	} 
+	else 
+		echo "<script>window.location.href='index.php'</script>";
 
-		$pages = 'cookie';
-		$lg = true;
+	$pages = 'cookie';
+	$lg = true;
 
-		include '../main/header.php';
-        include '../main/footer.php';
-    ?>
+	include '../main/header.php';
+	include '../main/footer.php';
+?>
